816150acff3530628d6e96adb769c20f56ede7a6
Derrick Junior <42792708+djunior411@users.noreply.github.com>
2019-01-24 14:00:45 -0600

    Add files via upload

---
+++ 99Lines.js
@@ -0,0 +1,15 @@
+var friend = ['Tom', 'Frank', 'Sarah', 'Betty', 'James']
+for (var i = 0; i < friend.length; i++){
+    console.log(friend[i]); 
+}
+
+
+
+for(var i = 0; i < 4; i++){
+    for(var j = 0; j < 3; j++){
+
+    }
+}
+document.write(friend [i]);
+document.write(friend [j]);
+
